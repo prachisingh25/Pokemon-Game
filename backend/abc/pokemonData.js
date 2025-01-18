3e4288b4ac0d6743a98d3fa001c9f6ad898c40fd
prachisingh25 <83166299+prachisingh25@users.noreply.github.com>
2025-01-18 13:36:53 +0530

    Add files via upload

---
+++ backend/abc/pokemonData.js
@@ -0,0 +1,94 @@
+const seedData = [
+  {
+    name: "Pikachu",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+    powers: "Electric,Quick Attack,Volt Tackle",
+  },
+  {
+    name: "Bulbasaur",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    powers: "Overgrow,Seed Bomb,Leech Seed",
+  },
+  {
+    name: "Charmander",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+    powers: "Blaze,Fire Spin,Dragon Breath",
+  },
+  {
+    name: "Squirtle",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+    powers: "Torrent,Aqua Jet,Shell Smash",
+  },
+  {
+    name: "Jigglypuff",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/39.png",
+    powers: "Cute Charm,Sing,Disarming Voice",
+  },
+  {
+    name: "Psyduck",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/54.png",
+    powers: "Damp,Psybeam,Confusion",
+  },
+  {
+    name: "Eevee",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/133.png",
+    powers: "Run Away,Swift,Baby-Doll Eyes",
+  },
+  {
+    name: "Snorlax",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png",
+    powers: "Immunity,Weight,Heavy Slam",
+  },
+  {
+    name: "Machop",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/66.png",
+    powers: "Guts,Dynamic Punch,Seismic Toss",
+  },
+  {
+    name: "Onix",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/95.png",
+    powers: "Sturdy,Iron Tail,Dragon Tail",
+  },
+  {
+    name: "Poliwag",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/60.png",
+    powers: "Water Absorb,Bubble Beam,Hypnosis",
+  },
+  {
+    name: "Geodude",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/74.png",
+    powers: "Rock Head,Magnitude,Smack Down",
+  },
+  {
+    name: "Magikarp",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/129.png",
+    powers: "Splash,Flail,Tackle",
+  },
+  {
+    name: "Growlithe",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/58.png",
+    powers: "Intimidate,Roar,Heat Wave",
+  },
+  {
+    name: "Arcanine",
+    image:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/59.png",
+    powers: "Burn Up,Extreme Speed,Flamethrower",
+  },
+];
+
+module.exports = seedData;
